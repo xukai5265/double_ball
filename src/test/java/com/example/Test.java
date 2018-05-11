76dc5139dc78714ed11cee9b865e15325316e288
xukai5265 <xukai5265@126.com>
2018-05-11 10:00:48 +0800

    t1

--- src/test/java/com/example/Test.java
+++ src/test/java/com/example/Test.java
@@ -7,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("test...");
         System.out.println("hello");
+        System.out.println("aaaa");
     }
 }

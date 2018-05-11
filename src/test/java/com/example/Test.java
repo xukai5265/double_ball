428d9057a6d84015789f67ef69a3307f7c4435ca
xukai5265 <xukai5265@126.com>
2018-05-11 10:50:18 +0800

    t4.1

--- src/test/java/com/example/Test.java
+++ src/test/java/com/example/Test.java
@@ -6,11 +6,5 @@ package com.example;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test...");
-        System.out.println("hello");
-        System.out.println("aaaa");
-        System.out.println("吴杰");
-        System.out.println("徐凯");
-        System.out.println("one");
-        System.out.println("Two");
     }
 }
